7bb653e06c24cca66f49b72046fc3d74ce663f26
mondlos <informatik.mueller104@gmail.com>
2019-02-11 11:24:52 +0100

    etliches

--- app/src/main/java/de/mueller104/informatik/dsbvertretung/LulListe.java
+++ app/src/main/java/de/mueller104/informatik/dsbvertretung/LulListe.java
@@ -14,12 +14,25 @@ import java.io.Reader;
 public class LulListe extends Form implements HandlesEventDispatching {
     private TableArrangement List;
     private Label Csv;
+    private Label Csv2;
     private Reader br;
     private String k;
 
     protected void $define(){
-        Csv = new Label(this);
         List = new TableArrangement(this);
+        List.WidthPercent(100);
+        List.HeightPercent(100);
+        List.Columns(2);
+        List.Rows(20);
+        Csv = new Label(List);
+        Csv2 = new Label(List);
+        Csv.Row(0);
+        Csv.Column(0);
+        Csv.TextAlignment(Label.ALIGNMENT_CENTER);
+        Csv2.Row(0);
+        Csv2.Column(1);
+
+
         try{
         InputStream raw = this.getAssets().open("lehrer.csv");
         k = convertStreamToString(raw);
@@ -28,7 +41,8 @@ public class LulListe extends Form implements HandlesEventDispatching {
             System.err.println("lehrer.csv konnte nicht gelesen werden");
             e.printStackTrace();
         }
-        //Csv.Text(k);
+        Csv.Text(k);
+        Csv2.Text(k);
 
     }
 
