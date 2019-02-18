f02199e916f348b338a3511e6e33d69a17b5644f
informatik-mueller <informatik.mueller104@gmail.com>
2019-02-18 06:53:07 +0100

    ftp zum Laufen gebracht

--- app/src/main/java/de/mueller104/informatik/dsbvertretung/LulListe.java
+++ app/src/main/java/de/mueller104/informatik/dsbvertretung/LulListe.java
@@ -1,9 +1,14 @@
 package de.mueller104.informatik.dsbvertretung;
 
+import com.google.appinventor.components.runtime.Clock;
+import com.google.appinventor.components.runtime.Component;
+import com.google.appinventor.components.runtime.EventDispatcher;
 import com.google.appinventor.components.runtime.Form;
 import com.google.appinventor.components.runtime.HandlesEventDispatching;
 import com.google.appinventor.components.runtime.Label;
 import com.google.appinventor.components.runtime.TableArrangement;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPClientConfig;
 import org.apache.commons.net.ftp.FTPFile;
@@ -12,22 +17,29 @@ import org.apache.commons.net.ftp.FTPReply;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
-//TODO: Download aus dem Internet
 
 public class LulListe extends Form implements HandlesEventDispatching {
     private TableArrangement List;
+    private Clock Timer1;
     private Label Csv;
     private Label Csv2;
-    private Reader br;
-    private String k;
-    private static final String FtpServerUrl = "leobraguinski.bplaced.net";
     private FTPClient client;
-    private FTPFile[] files;
-    private static final String lulUrl = "/lulUrl";
+    private List<String> fileNames;
+    private String CsvTable;
+
+    private static final String FtpServerUrl = "leobraguinski.bplaced.net";
+    private static final String FtpBenutzer = "leonardobraguinski";
+    private static final String FtpPasswort = "blabla";
+    private static final String DateiName = "lehrer.csv";
+    private static final String DateiPfad = "/lulListe";
 
     protected void $define(){
+        Timer1 = new Clock(this);
+        Timer1.TimerInterval(50);
+        Timer1.TimerEnabled(true);
         List = new TableArrangement(this);
         List.WidthPercent(100);
         List.HeightPercent(100);
@@ -40,31 +52,63 @@ public class LulListe extends Form implements HandlesEventDispatching {
         Csv.TextAlignment(Label.ALIGNMENT_CENTER);
         Csv2.Row(0);
         Csv2.Column(1);
+        EventDispatcher.registerEventForDelegation(this, "ok", "Timer");
+    }
 
+    public boolean dispatchEvent(Component component, String componentName, String eventName, Object[] params){
+        if(component.equals(Timer1) && eventName.equals("Timer")){
+            CsvTable = retrieveFTP();
+            Timer1.TimerEnabled(false);
+            Csv2.Text(CsvTable);
+            return true;
+        }
+        return false;
+    }
+
+    protected String retrieveFTP(){
+        String InStr = "";
 
         try{
             client = new FTPClient();
             client.connect(FtpServerUrl);
-                 if(!FTPReply.isPositiveCompletion(client.getReplyCode())){
-                     System.err.println("Etwas ist schief gelaufen..");
-                 }
-                 boolean success = client.login("leobraguinski", "blabla");
-                 if(success){
-                     Csv.Text(client.listFiles("/")[2].getName());
-                 }
+            if(!FTPReply.isPositiveCompletion(client.getReplyCode())){
+                System.err.println("Etwas ist schief gelaufen..");
+            }
+            boolean success = client.login(FtpBenutzer, FtpPasswort);
+            if(success) {
+                FTPFile[] files = client.listFiles();
+                fileNames = new ArrayList<>();
+                for (FTPFile file : files) {
+                    String name = file.getName();
+                    if (!name.equals("...") && !name.equals("..") && !name.equals("")) {
+                        fileNames.add(file.getName());
+                    }
+                }
+                if(fileNames.contains(DateiName)){
+                /*int i = 0;
+                for (String name : fileNames){
+                    if (name.equals(DateiName)){
+                        break;
+                    }
+                    i++;
+                }*/
 
-            InputStream raw = this.getAssets().open("lehrer.csv");
-            Scanner scanner = new java.util.Scanner(raw).useDelimiter("\\A");
-            if(scanner.hasNext())
-                k = scanner.next();
-            else k = "";
+                InputStream raw = client.retrieveFileStream(DateiName);
+                Scanner scanner = new java.util.Scanner(raw).useDelimiter("\\A");
+                if(scanner.hasNext()){
+                    InStr = scanner.next();
+                }
+
+                }
+                else InStr = "";
+            }
+            return InStr;
         }
+
         catch(IOException e){
-            System.err.println("lehrer.csv konnte nicht gelesen werden");
+            System.err.println(DateiName + " konnte nicht gelesen werden");
             e.printStackTrace();
+            return null;
         }
-        //Csv.Text(k);
-        Csv2.Text(k);
-
     }
 }
